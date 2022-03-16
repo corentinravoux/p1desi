556985d545638a77d595e1074dc5aa77e97fe32a
corentinravoux <corentin.ravoux.research@gmail.com>
2022-03-16 10:59:45 +0100

    Improvement of interface

--- p1desi/interface.py
+++ p1desi/interface.py
@@ -64,108 +64,125 @@ def main(input_file):
     config.read(input_file)
 
     main_config = config["main"]
-    main_path = os.path.abspath(main_config["path"])
+    path_config = config["path"]
+    main_path = os.path.abspath(path_config["path"])
     os.makedirs(main_path,exist_ok=True)
 
-    (pk,pk_sb,outname) = bookkeeping.return_pk_path_interface(main_config)
-    mean_config = config["compute mean"]
-    plot_config = config["plot power"]
-    plot_noise_config = config["plot noise"]
+    (pk,pk_sb,outname) = bookkeeping.return_pk_path_interface(path_config)
 
     if(main_config.getboolean("compute_mean")):
+        print("Treating path: ",pk)
+        mean_config = config["compute mean"]
         compute_mean(pk,mean_config,main_config)
 
-    path_plot = os.path.join(main_path,plot_config.getstr("path_plot"))
-    if(main_config.getboolean("plot_power")):
-        os.makedirs(path_plot,exist_ok=True)
-        plot(pk,pk_sb,path_plot,plot_config,main_config,outname)
+    velunits = main_config.getboolean("velunits")
+    logsample=main_config.getboolean("logsample")
+    mean_pk = os.path.join(pk,bookkeeping.return_mean_pk_name(velunits,logsample))
+    if(pk_sb is not None):
+        mean_pk_sb = os.path.join(pk_sb,bookkeeping.return_mean_pk_name(velunits,logsample))
+    else:
+        mean_pk_sb = None
+    outname = f"{outname}_unit{'kms' if velunits else 'A'}"
 
-    path_plot_noise = os.path.join(main_path,plot_noise_config.getstr("path_plot_noise"))
+    if(main_config.getboolean("plot_power")):
+        plot_config = config["plot power"]
+        path_plot = os.path.join(main_path,plot_config.getstr("path_plot"))
+        os.makedirs(path_plot,exist_ok=True)
+        print("Plotting pk1d for path: ",pk)
+        plot(mean_pk,mean_pk_sb,path_plot,plot_config,main_config,outname)
+
     if(main_config.getboolean("plot_noise")):
+        plot_noise_config = config["plot noise"]
+        path_plot_noise = os.path.join(main_path,plot_noise_config.getstr("path_plot_noise"))
         os.makedirs(path_plot_noise,exist_ok=True)
-        plot_noise(pk,path_plot_noise,plot_noise_config,main_config,outname)
+        print("Plotting noise study for path: ",pk)
+        plot_noise(mean_pk,path_plot_noise,plot_noise_config,main_config,outname)
+
+
+    if(main_config.getboolean("plot_metals")):
+        plot_metals_config = config["plot metals"]
+        path_plot_metals = os.path.join(main_path,plot_metals_config.getstr("path_plot_metals"))
+        os.makedirs(path_plot_metals,exist_ok=True)
+        print("Plotting metals study for path: ",pk)
+        plot_metals(mean_pk,path_plot_metals,plot_metals_config,main_config,outname)
 
 
 
 
 
 def compute_mean(pk,mean_config,main_config):
-    print("Treating path: ",pk)
     treatpk.compute_Pk_means_parallel(pk,
                                       mean_config.getdict("args_k_array"),
-                                      np.array(mean_config.gettuplefloat("zbins")),
+                                      np.array(main_config.gettuplefloat("zbins")),
                                       searchstr=mean_config.getstr("searchstr"),
                                       ncpu=mean_config.getint("ncpu"),
                                       overwrite=mean_config.getboolean("overwrite"),
                                       velunits=main_config.getboolean("velunits"),
                                       debug=mean_config.getboolean("debug"),
                                       nomedians=mean_config.getboolean("nomedians"),
-                                      logsample=mean_config.getboolean("logsample"))
+                                      logsample=main_config.getboolean("logsample"))
 
 
-def plot(pk,pk_sb,path_plot,plot_config,main_config,outname):
-    print("Plotting pk1d for path: ",pk)
-    mean_pk = os.path.join(pk,f"mean_Pk1d_par{'_vel' if main_config.getboolean('velunits') else ''}.fits.gz")
-    velunits = main_config.getboolean("velunits")
-    comparison_str = plot_config.getstr("comparison_str")
-    substract_sb = plot_config.getboolean("substract_sb")
-    region_sb = plot_config.getstr("region_sb")
-    plot_args = plot_config.getdict("plot_args")
+def plot(mean_pk,mean_pk_sb,path_plot,plot_config,main_config,outname):
+    if(plot_config.getboolean("substract_sb")):
+        substract_data = mean_pk_sb
+        outname =  f"{outname}_{plot_config.getstr('region_sb')}_substracted"
+    else:
+        substract_data = None
+
     comparison_model_file=list(plot_config.gettuplestr("comparison_model_file"))
     if(len(comparison_model_file) == 1):
         comparison_model_file = comparison_model_file[0]
-    outname = os.path.join(path_plot,f"{outname}_model{comparison_str}_unit{'kms' if velunits else 'A'}")
+    outname = os.path.join(path_plot,f"{outname}_model{plot_config.getstr('comparison_str')}")
 
-    if(plot_config.getboolean("plot_pk")):
-        data = plotpk.read_pk_means(mean_pk)
-        if(substract_sb):
-            mean_pk_sb = os.path.join(pk_sb,f"mean_Pk1d_par{'_vel' if main_config.getboolean('velunits') else ''}.fits.gz")
-            substract_data = mean_pk_sb
-            outname =  f"{outname}_{region_sb}_substracted"
-        else:
-            substract_data = None
-        plotpk.plot_data(data,
-                         np.array(plot_config.gettuplefloat("zbins_plot")),
-                         outname,
-                         plot_P=plot_config.getboolean("plot_P"),
-                         comparison=plot_config.getstr("pk_comparison"),
-                         comparison_model=plot_config.getstr("model_comparison"),
-                         comparison_model_file=comparison_model_file,
-                         plot_diff=plot_config.getboolean("plot_diff_model"),
-                         substract_sb=substract_data,
-                         **plot_args)
+    plotpk.plot_data(plotpk.read_pk_means(mean_pk),
+                     np.array(main_config.gettuplefloat("zbins_plot")),
+                     outname,
+                     plot_P=plot_config.getboolean("plot_P"),
+                     comparison=plot_config.getstr("pk_comparison"),
+                     comparison_model=plot_config.getstr("model_comparison"),
+                     comparison_model_file=comparison_model_file,
+                     plot_diff=plot_config.getboolean("plot_diff_model"),
+                     substract_sb=substract_data,
+                     **plot_config.getdict("plot_args"))
 
 
-def plot_noise(pk,path_plot_noise,plot_noise_config,main_config,outname):
-    # CR - move side_band in a metals study plotting
-    print("Plotting noise study for path: ",pk)
-    mean_pk = os.path.join(pk,f"mean_Pk1d_par{'_vel' if main_config.getboolean('velunits') else ''}.fits.gz")
-    velunits = main_config.getboolean("velunits")
-    outname = os.path.join(path_plot_noise,f"{outname}_unit{'kms' if velunits else 'A'}")
-    zbins_plot = np.array(plot_noise_config.gettuplefloat("zbins_plot"))
-
-    plot_args_noise = plot_noise_config.getdict("plot_args_noise")
-
-
+def plot_noise(mean_pk,path_plot_noise,plot_noise_config,main_config,outname):
+    outname = os.path.join(path_plot_noise,outname)
     data = plotpk.read_pk_means(mean_pk)
 
-    if(plot_noise_config.getboolean("plot_noise_study")):
-        plotpk.plot_noise_study(data,
-                                zbins_plot,
-                                outname,
-                                plot_noise_config.getstr("k_units_noise_study"),
-                                plot_noise_config.getboolean("use_diff_noise"),
-                                plot_noise_config.getboolean("plot_noise_ratio"),
-                                plot_noise_config.getboolean("plot_noise_comparison_mean_k"),
-                                plot_noise_config.getboolean("plot_side_band"),
-                                side_band_comp=plot_noise_config.getstr("side_band_comp"),
-                                side_band_legend=plot_noise_config.gettuplestr("side_band_legend"),
-                                fit_asymptote_ratio= plot_noise_config.getboolean("fit_asymptote_ratio"),
-                                **plot_args_noise)
+    plotpk.plot_noise_study(data,
+                            np.array(main_config.gettuplefloat("zbins_plot")),
+                            outname,
+                            plot_noise_config.getstr("k_units_noise_study"),
+                            plot_noise_config.getboolean("use_diff_noise"),
+                            plot_noise_config.getboolean("plot_noise_ratio"),
+                            plot_noise_config.getboolean("plot_noise_comparison_mean_k"),
+                            plot_noise_config.getboolean("plot_side_band"),
+                            side_band_comp=plot_noise_config.getstr("side_band_comp"),
+                            side_band_legend=plot_noise_config.gettuplestr("side_band_legend"),
+                            fit_asymptote_ratio= plot_noise_config.getboolean("fit_asymptote_ratio"),
+                            **plot_noise_config.getdict("plot_args_noise"))
 
 
     if(plot_noise_config.getboolean("plot_noise_comparison_mean_z")):
         plotpk.compute_and_plot_mean_z_noise_power(data,
-                                                   zbins_plot,
+                                                   np.array(main_config.gettuplefloat("zbins_plot")),
                                                    outname,
-                                                   **plot_args_noise)
+                                                   **plot_noise_config.getdict("plot_args_noise"))
+
+
+def plot_metals(mean_pk,path_plot_metals,plot_metals_config,main_config,outname):
+    outname = os.path.join(path_plot_metals,outname)
+    data = plotpk.read_pk_means(mean_pk)
+
+    plotpk.plot_metal_study(data,
+                            np.array(main_config.gettuplefloat("zbins_plot")),
+                            outname,
+                            plot_metals_config.getstr("k_units_metals_study"),
+                            plot_metals_config.getboolean("use_diff_noise"),
+                            plot_metals_config.getboolean("plot_side_band"),
+                            side_band_comp=plot_metals_config.getstr("side_band_comp"),
+                            side_band_legend=plot_metals_config.gettuplestr("side_band_legend"),
+                            side_band_fitpolynome=plot_metals_config.getboolean("side_band_fitpolynome"),
+                            **plot_metals_config.getdict("plot_args_metals"))
