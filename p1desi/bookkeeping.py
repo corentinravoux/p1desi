4a2a548b89b6f085a242acee26364d9fa3281483
corentinravoux <corentin.ravoux.research@gmail.com>
2022-03-17 15:57:42 +0100

    New P1D picca standard: running SNR>1 deltas everytime

--- p1desi/bookkeeping.py
+++ p1desi/bookkeeping.py
@@ -1,17 +1,17 @@
 import os
 
-def return_folder_name(region,snr_cut,lines_name,catalog_name,dla_name,bal,noise_estimate,suffix):
+def return_folder_name(region,lines_name,catalog_name,dla_name,bal,noise_estimate,suffix):
     if(suffix == "None")|(suffix == None):
         suffix = ""
-    folder_name=f"{region}_SNRcut{snr_cut}_noise{noise_estimate}_lines{lines_name}_cat{catalog_name}_dla{dla_name}_bal{bal}{suffix}"
+    folder_name=f"{region}_noise{noise_estimate}_lines{lines_name}_cat{catalog_name}_dla{dla_name}_bal{bal}{suffix}"
     return(folder_name)
 
 def return_delta_path(pk_path,folder_name):
     pk = os.path.join(pk_path,f"p1d_{folder_name}","deltas")
     return(pk)
 
-def return_pk_path(pk_path,folder_name):
-    pk = os.path.join(pk_path,f"p1d_{folder_name}","pk1d")
+def return_pk_path(pk_path,folder_name,snr_cut):
+    pk = os.path.join(pk_path,f"p1d_{folder_name}",f"pk1d_SNRcut{snr_cut}")
     return(pk)
 
 def return_pk_path_interface(main_config):
@@ -19,7 +19,6 @@ def return_pk_path_interface(main_config):
     use_bookkeeping = main_config.getboolean("use_bookkeeping")
     if(use_bookkeeping):
         region = main_config.getstr("region")
-        snr_cut = main_config.getint("snr_cut")
         lines_name = main_config.getstr("lines_name")
         catalog_name = main_config.getstr("catalog_name")
         dla_name = main_config.getstr("dla_name")
@@ -27,16 +26,18 @@ def return_pk_path_interface(main_config):
         noise_estimate = main_config.getstr("noise_estimate")
         suffix = main_config.getstr("suffix")
 
-        folder_name=return_folder_name(region,snr_cut,lines_name,catalog_name,dla_name,bal,noise_estimate,suffix)
-        outname = folder_name
-        pk = return_pk_path(pk_path,folder_name)
+        folder_name=return_folder_name(region,lines_name,catalog_name,dla_name,bal,noise_estimate,suffix)
+
+        snr_cut = main_config.getint("snr_cut")
+        outname = f"{folder_name}_SNRcut{snr_cut}"
+        pk = return_pk_path(pk_path,folder_name,snr_cut)
 
         region_sb = main_config.getstr("region_sb")
         if(region_sb is None):
             pk_sb = None
         else:
             folder_name_sb=return_folder_name(region_sb,snr_cut,lines_name,catalog_name,dla_name,bal,noise_estimate,suffix)
-            pk_sb = return_pk_path(pk_path,folder_name_sb)
+            pk_sb = return_pk_path(pk_path,folder_name_sb,snr_cut)
     else:
         pk = main_config.getstr("abs_pk_path")
         pk_sb = main_config.getstr("abs_pk_path_sb")
--- scripts/p1desi_bookkeeping.py
+++ scripts/p1desi_bookkeeping.py
@@ -5,7 +5,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='p1d bookkeeping')
 parser.add_argument('--region', help='Region on which to compute p1d',default="lya",required=False)
-parser.add_argument('--snr-cut', help='SNR cut applied',default=1,required=False)
 parser.add_argument('--lines-name', help='Sky line file name', default=None,required=False)
 parser.add_argument('--catalog-name', help='Quasar catalog name used', default=None,required=False)
 parser.add_argument('--dla-name', help='DLA catalog name used', default=None,required=False)
@@ -16,7 +15,6 @@ parser.add_argument('--suffix', help='Optional suffix', default=None,required=Fa
 args = vars(parser.parse_args())
 
 folder_name = bookkeeping.return_folder_name(args["region"],
-                                             args["snr_cut"],
                                              args["lines_name"],
                                              args["catalog_name"],
                                              args["dla_name"],
