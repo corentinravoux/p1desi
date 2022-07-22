5117d6efedd60aca46c8cd25e5108946ea796d58
corentinravoux <corentin.ravoux.research@gmail.com>
2022-07-22 17:24:36 +0200

    adding of constants

--- p1desi/utils.py
+++ p1desi/utils.py
@@ -1,5 +1,26 @@
+import numpy as np
 
 lambdaLy = 1215.673123130217
+
+speed_light = 2.99792458 * 10**5
+
+si4_1 = 1393.76
+si4_2 = 1402.77
+c4_1 = 1548.202
+c4_2 = 1550.774
+
+vsi4 = speed_light * np.log(si4_2 / si4_1)
+lsi4=(si4_2 - si4_1)
+ksi4=2*np.pi/(si4_2 - si4_1)
+ksi4_speed = 2*np.pi/vsi4
+
+
+vc4 = speed_light * np.log(c4_2 / c4_1)
+lc4=(c4_2 - c4_1)
+kc4=2*np.pi/(c4_2 - c4_1)
+kc4_speed = 2 * np.pi/vc4
+
+
 
 
 def return_key(dictionary,string,default_value):
