cdc73798a78300883af3e28bdb46cdffac610949
corentin.ravoux <corentin.ravoux01@hotmail.fr>
2022-10-28 16:39:29 +0200

    add short script for catalogs

---
+++ scripts/p1desi_catalog_number.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import argparse
+from p1desi import catalogs
+parser = argparse.ArgumentParser(description='p1d catalogs')
+parser.add_argument('--name', help='Catalog name',required=True)
+parser.add_argument('--type', help='Catalog type',default="QSO",required=False)
+parser.add_argument('--extname', help='Special extname',default=None,required=False)
+
+args = vars(parser.parse_args())
+
+catalogs.get_number(args["name"], type=args["type"],extname=args["extname"])
