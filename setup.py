18b6f4cace3a40b60c282723387ecf32cf8b91d9
corentinravoux <corentin.ravoux.research@gmail.com>
2023-12-01 16:40:39 +0100

    format

--- setup.py
+++ setup.py
@@ -4,30 +4,28 @@
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pip install twine
 
+import glob
 import io
 import os
 import sys
-import glob
 from shutil import rmtree
 
-from setuptools import setup, Command
+from setuptools import Command, setup
 
 # Package meta-data.
-NAME = 'p1desi'
-DESCRIPTION = 'P1D utilities for DESI SV measurement'
-URL = 'https://github.com/corentinravoux/p1desi'
-EMAIL = 'corentin.ravoux01@hotmail.fr'
-AUTHOR = 'Corentin Ravoux'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0'
+NAME = "p1desi"
+DESCRIPTION = "P1D utilities for DESI SV measurement"
+URL = "https://github.com/corentinravoux/p1desi"
+EMAIL = "corentin.ravoux01@hotmail.fr"
+AUTHOR = "Corentin Ravoux"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "1.0"
 
-scripts = glob.glob('scripts/*') + glob.glob('bash/*')
-packages = ['p1desi']
+scripts = glob.glob("scripts/*") + glob.glob("bash/*")
+packages = ["p1desi"]
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-     'fitsio', 'numpy', 'scipy','matplotlib'
-]
+REQUIRED = ["fitsio", "numpy", "scipy", "matplotlib"]
 
 # What packages are optional?
 EXTRAS = {
@@ -44,30 +42,30 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
+    with open(os.path.join(here, NAME, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -77,20 +75,20 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
 
         sys.exit()
 
@@ -98,41 +96,40 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-#    packages=find_packages(exclude=('tests',)),
-    packages = packages,
+    #    packages=find_packages(exclude=('tests',)),
+    packages=packages,
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-    scripts = scripts,
-
-    #entry_points={
+    scripts=scripts,
+    # entry_points={
     #    'console_scripts': ['mycli=mymodule:cli'],
-    #},
-#    package_data={'lslyatomo' :}
+    # },
+    #    package_data={'lelantos' :}
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     # $ setup.py publish support.
     cmdclass={
-        'upload': UploadCommand,
+        "upload": UploadCommand,
     },
 )
